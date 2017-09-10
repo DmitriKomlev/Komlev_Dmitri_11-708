c87a103e174bc102c3f73a34e87f6a6f6f232304
Komlev_Dmitri_11-708 <31667843+DmitriKomlev@users.noreply.github.com>
2017-09-11 00:41:09 +0300

    Add files via upload
    added the first task of semestruction

---
+++ 2017/FALL2017/SEM/Sem1Ex1/Sem1Ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem1Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your ticket number: ");
+            string NumTicket=Console.ReadLine();
+            int FirstPart = NumTicket[0] + NumTicket[2] + NumTicket[4]; //присваиваем переменной FisrtPart сумму 1,3,5 цифр номера билета
+            int SecondPart = NumTicket[1] + NumTicket[3] + NumTicket[5];//присваиваем переменной SecondPart сумму 2,4,6 цифр номера билета
+            if (FirstPart==SecondPart) //Сравниваем полученнные суммы
+            {
+                Console.WriteLine("Congratulations! You are got a lucky ticket!");
+            }
+            else
+            {
+                Console.WriteLine("Unfortunately, your ticket is not happy");
+            }
+            Console.ReadLine();
+        }
+    }
+}
