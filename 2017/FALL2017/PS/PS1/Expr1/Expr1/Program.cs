8dcf4bacc6b508e8a5016c89e7820007cdddb065
Дмитрий <komlev.dmit01@gmail.com>
2017-09-13 23:08:00 +0300

    добавлено решение первой задачи

---
+++ 2017/FALL2017/PS/PS1/Expr1/Expr1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("a= ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b= ");
+            b = int.Parse(Console.ReadLine());
+            a = a - b; //A= delta(a,b)
+            b = b + a; // b = b + delta
+            a = b - a; // a = b- delta
+            Console.WriteLine("a = " + a + " b = " + b);
+            Console.ReadLine();
+            int c; // альтернативное решение
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("a = " + a + " b = " + b);
+            Console.ReadLine();
+        }
+    }
+}
