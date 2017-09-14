4520edda479c9477df59aea7f65d800c55e44e48
Дмитрий <komlev.dmit01@gmail.com>
2017-09-14 12:07:31 +0300

    Добавлено 3 задание. Решил Комлев Д

---
+++ 2017/FALL2017/PS/PS1/Expr3/Expr3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+            if (a<12) 
+            {                           //вычисляем угол для чисел <12
+                if (a<=6) 
+                {
+                    Console.WriteLine(a*30);
+                }
+                else
+                {
+                    Console.WriteLine((12-a) * 30);
+                }
+            }
+            else
+            {                           //вычисляем угол для чисел >12
+                a -= 12;
+                if (a <= 6)
+                {
+                    Console.WriteLine(a * 30);
+                }
+                else
+                {
+                    Console.WriteLine((12 - a) * 30);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
