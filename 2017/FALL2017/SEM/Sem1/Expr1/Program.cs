0a1e79b5ef9c7105ea5c338654d8b1b54682c2bb
Дмитрий <komlev.dmit01@gmail.com>
2017-09-21 19:11:57 +0300

    добавлено решение 1 задачи 1 семинара

---
+++ 2017/FALL2017/SEM/Sem1/Expr1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr1
+{
+    class Program
+    {//перестановка чисел местами
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("a= ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b= ");
+            b = int.Parse(Console.ReadLine());
+            a = a - b; //без использования 3ей переменной
+            b = b + a; 
+            a = b - a; 
+            Console.WriteLine("a = " + a + " b = " + b);
+            Console.ReadLine();
+            int c; // альтернативное решение
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("a = " + a + " b = " + b);
+            Console.ReadLine();
+        }
+    }
+}
