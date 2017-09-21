aedcb4c3da2f76062aea92922667d0e48f3ca190
Дмитрий <komlev.dmit01@gmail.com>
2017-09-21 19:22:24 +0300

    добавлено решение 2 задачи 1 семинара

---
+++ 2017/FALL2017/SEM/Sem1/Expr2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr2
+{
+    class Program
+    {//"перевернуть" 3хзначное число
+        static void Main(string[] args)
+        {
+            int a,x; // универсальный код( подходит не только для 3х значных чисел)
+            string a1;
+            a = int.Parse(Console.ReadLine());
+            a1 = a.ToString();
+
+            x = a1.Length;
+            string a2=null;
+            for (int i = x - 1; i >= 0; i--)
+            {
+                a2 = a2 + a1[i];
+            }
+            a = int.Parse(a2);
+            Console.WriteLine(a);
+            Console.ReadLine();
+            //int a;                    // подходит только для 3хзначных чисел без изменения а
+            //a = 123;
+            //int b = (a / 100);
+            //b = (a / 100) + (((a % 100) / 10) * 10) + ((a % 10) * 100);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.ReadLine();
+
+        }
+    }
+}
