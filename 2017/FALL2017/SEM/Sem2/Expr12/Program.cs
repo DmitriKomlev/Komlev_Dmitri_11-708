98795edb0a54c00b1347aa9f8aacf955d16826d0
Дмитрий <komlev.dmit01@gmail.com>
2017-09-21 20:49:54 +0300

    добавлено решение 3 задачи 2 семинара

---
+++ 2017/FALL2017/SEM/Sem2/Expr12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            double h, t, v, x;
+            h = double.Parse(str.Substring(0, str.IndexOf(' ')));
+            str = str.Substring(str.IndexOf(' ') + 1);
+            t = double.Parse(str.Substring(0, str.IndexOf(' ')));
+            str = str.Substring(str.IndexOf(' ') + 1);
+            v = double.Parse(str.Substring(0, str.IndexOf(' ')));
+            str = str.Substring(str.IndexOf(' ') + 1);
+            x = double.Parse(str);
+            double tmax, tmin;
+            if(h/x>t)
+            {
+                tmax = t;
+            }
+            else
+            {
+                tmax = h / x;
+            }
+
+            tmin = (h - x * t) / (v - x); ;
+            Console.WriteLine(tmin + " " + tmax);
+            Console.ReadLine();
+
+        }
+    }
+}
