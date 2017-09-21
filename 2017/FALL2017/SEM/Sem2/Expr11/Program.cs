152f77b742c0510f2f0b0531fb0711f7679abbf1
Дмитрий <komlev.dmit01@gmail.com>
2017-09-21 20:19:59 +0300

    добавлено решение 2 задачи 2 семинара

---
+++ 2017/FALL2017/SEM/Sem2/Expr11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr10
+{
+    class Program
+    {//Дано время в часах и минутах. Найти угол от часовой к минутной стрелке на обычных часах.
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+            if (hour >= 12) hour -= 12;
+            min *= 6;
+            hour *= 30;
+            int delta;
+            if (hour>min)
+            {
+                delta = hour - min;
+            }
+            else
+            {
+                delta = min - hour;
+            }
+            if (delta > 180) delta = 360 - delta;
+            Console.WriteLine(delta);
+            Console.ReadLine();
+        }
+    }
+}
