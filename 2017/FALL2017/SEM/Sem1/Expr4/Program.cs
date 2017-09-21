1fcf73b154037d4fbda37a5e6ad07a45a2eeff83
Дмитрий <komlev.dmit01@gmail.com>
2017-09-21 19:33:28 +0300

    добавлено решение 4 задачи 1 семинара

---
+++ 2017/FALL2017/SEM/Sem1/Expr4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr4
+{
+    class Program
+    {//Найти количество чисел меньших N, которые имеют простые делители X или Y.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter X:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Y:");
+            int y = int.Parse(Console.ReadLine());
+            int k=0;
+            //for (int i =1; i<=n; i++)
+            //{
+            //    if ((i%x==0) | (i%y==0))
+            //    {
+            //        k++;
+            //    }
+            //}
+            int temp;
+            temp = ((n-1) / x)+((n-1)/y)-(n-1)/(x*y);
+            Console.WriteLine(temp);
+            Console.ReadLine();
+        }
+    }
+}
