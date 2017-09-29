4ec15bd02c575eb583c2b791d1cd8f9aee9dcc1c
Дмитрий <komlev.dmit01@gmail.com>
2017-09-30 01:59:31 +0300

    добавлено решение 3 задачи 3 семинара

---
+++ 2017/FALL2017/SEM/Sem3/Cound3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cound3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfTicket = int.Parse(Console.ReadLine());
+            if (((numOfTicket + 1) % 10 + ((numOfTicket + 1) / 10) % 10 + ((numOfTicket + 1) / 100) % 10 == ((numOfTicket + 1) / 1000) % 10 + ((numOfTicket + 1) / 10000) % 10 + ((numOfTicket + 1) / 100000)) ||
+                    ((numOfTicket - 1) % 10 + ((numOfTicket - 1) / 10) % 10 + ((numOfTicket - 1) / 100) % 10 == ((numOfTicket - 1) / 1000) % 10 + ((numOfTicket - 1) / 10000) % 10 + ((numOfTicket - 1) / 100000)))
+                Console.WriteLine("true");
+            else
+                Console.WriteLine("true");
+            Console.ReadKey();
+        }
+    }
+}
