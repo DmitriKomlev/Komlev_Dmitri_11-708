5dbc93a4f749654e84fa825292182b6c1673427b
Дмитрий <komlev.dmit01@gmail.com>
2017-09-30 11:18:42 +0300

    добавлено решение 7 задачи 3 семинара

---
+++ 2017/FALL2017/SEM/Sem3/Cound7.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cound7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("X=");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("N=");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Y=");
+            double y = double.Parse(Console.ReadLine());
+            int n1;
+            n1 =(int)Math.Floor((x - y) * n / (y - 1)) + 1;
+            Console.WriteLine(n1);
+            Console.ReadLine();
+        }
+    }
+}
