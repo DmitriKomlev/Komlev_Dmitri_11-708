ea7c39eceb0e2ef9ff01db54601e97163f91be19
Дмитрий <komlev.dmit01@gmail.com>
2017-10-08 16:03:33 +0300

    добавлено решение 3 задачи 1 семестровки

---
+++ 2017/FALL2017/PS/ps1/3dTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem1Ex3
+{
+    class Program
+    {
+        //Найти сумму цифр в k-ичной системе счисления (k от 2 до 10) десятичного натурального числа n (n<=10^9)
+        static long TranslationFrom10(long number, int degree)
+        {
+            long sum=0;
+            while (number != 0)
+            {
+                sum += (number % degree);
+                number = number / degree;
+                if (number == 0) sum += (number / degree);
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            int degree = int.Parse(Console.ReadLine());
+            
+           
+
+            Console.WriteLine(TranslationFrom10(number,degree));
+            Console.ReadLine();
+            
+        }
+    }
+}
