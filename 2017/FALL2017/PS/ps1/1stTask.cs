db0a1de14612d9d63f56d946ce9a82e9f0347b1e
Дмитрий <komlev.dmit01@gmail.com>
2017-10-08 16:02:46 +0300

    добавлено решение 1 задачи 1 семестровки

---
+++ 2017/FALL2017/PS/ps1/1stTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem1Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Проверить, является ли билет счастливым по-питерски (abcdef=>a+c+e=b+d+f)
+            Console.Write("Enter your ticket number: ");
+            int numTicket=int.Parse(Console.ReadLine());
+            int firstPart = numTicket%10 + (numTicket/100)%10 + (numTicket/10000)%10; //присваиваем переменной FisrtPart сумму 1,3,5 цифр номера билета
+            int secondPart = (numTicket/10)%10 + (numTicket/1000)%10 + numTicket/100000;//присваиваем переменной SecondPart сумму 2,4,6 цифр номера билета
+            if (firstPart==secondPart) //Сравниваем полученнные суммы
+            {
+                Console.WriteLine("Congratulations! You are got a lucky ticket!");
+            }
+            else
+            {
+                Console.WriteLine("Unfortunately, your ticket is not happy");
+            }
+            Console.ReadLine();
+        }
+    }
+}
