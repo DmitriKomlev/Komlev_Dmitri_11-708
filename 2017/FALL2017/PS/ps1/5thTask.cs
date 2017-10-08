6db13aab2f0f3a0ca51988a4cb76bc26b54cce67
Дмитрий <komlev.dmit01@gmail.com>
2017-10-08 16:04:27 +0300

    добавлено решение 5 задачи 1 семестровки

---
+++ 2017/FALL2017/PS/ps1/5thTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem1Ex5
+{
+    class Program
+    {
+        //Посчитать, сколько совершенных чисел от 0 до 1000000. Число n - совершенное, если сумма его делителей равна 2n.
+        static void Main(string[] args)
+        {
+            int p=1,k=0,k1=0;
+            double num,tempNum;
+            num = (Math.Pow(2, p-1))*(Math.Pow(2,p)-1);
+            while(num<=1000000)
+            {
+                tempNum = Math.Pow(2, p) - 1;
+                k = 0;
+                for (int i = 1; i<=tempNum/2;i++)
+                {
+                    if (tempNum % i == 0) k++;
+                    if (k > 1) break;
+                }
+                if (k + 1 == 2) k1++;
+                p++;
+                num = (Math.Pow(2, p - 1)) * (Math.Pow(2, p) - 1);
+            }
+            Console.WriteLine(k1);
+            Console.ReadLine();
+        }
+    }
+}
