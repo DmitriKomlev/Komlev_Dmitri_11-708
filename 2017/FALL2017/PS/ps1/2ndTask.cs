b44022d14fb2c73ed51106cfc4186bf78ec53086
Дмитрий <komlev.dmit01@gmail.com>
2017-10-08 16:01:59 +0300

    добавлено решение 2 задачи 1 семестровки

---
+++ 2017/FALL2017/PS/ps1/2ndTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem1Ex2
+{
+    class Program
+    {
+        public static double Exponentation(double foundation, int exponent)
+        {//возведение в степень не кратную 2
+            double temp = 1;
+            for (int i =1; i<=exponent;i++)
+
+            {
+                temp *= foundation;
+            }
+            return temp;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter foundation of degree: ");
+            double foundation = Double.Parse(Console.ReadLine());
+            Console.Write("Enter exponent: ");
+            int exponent = int.Parse(Console.ReadLine());
+            while( exponent %2 ==0)
+            {
+                
+                foundation *= foundation;
+                exponent /= 2;
+            }
+            foundation = Exponentation(foundation, exponent);
+            Console.WriteLine(foundation);
+            Console.ReadLine();
+
+        }
+    }
+}
