2703477e6ebe14fc6dee476d4e5d4577446d47dc
DmitriKomlev <komlev.dmit01@gmail.com>
2018-02-27 15:13:50 +0300

    на паре

---
+++ 2017/SPRING2018/PS/27_02_18.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] array = new int[count];
+            for (int i = 0; i < count; i++)
+                array[i] = i;
+            Console.WriteLine("enter numbers");
+            do
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                if (array[a] != array[b])
+                    break;
+                for(int j = 0; j<count; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
